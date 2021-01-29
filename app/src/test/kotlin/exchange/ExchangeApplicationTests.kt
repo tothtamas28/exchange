c6a001d778ff41062d1ffff43977aca2143f9e4a
Tamas Toth <tamas.toth@vacuumlabs.com>
2021-01-29 14:35:38 +0100

    Fix failing default unit test
    Disabled auto-configuration for Spring JPA and mocked Repository beans.

--- app/src/test/kotlin/exchange/ExchangeApplicationTests.kt
+++ app/src/test/kotlin/exchange/ExchangeApplicationTests.kt
@@ -1,10 +1,25 @@
 package exchange
 
+import exchange.data.BalanceRepository
+import exchange.data.OrderRepository
+import exchange.data.UserRepository
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.boot.test.mock.mockito.MockBean
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExchangeApplicationTests {
+
+	@MockBean
+	private lateinit var userRepository : UserRepository
+
+	@MockBean
+	private lateinit var balanceRepository : BalanceRepository
+
+	@MockBean
+	private lateinit var orderRepository : OrderRepository
 
 	@Test
 	fun contextLoads() {
