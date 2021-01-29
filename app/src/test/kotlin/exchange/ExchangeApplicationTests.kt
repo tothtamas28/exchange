071d642eebadece3ee3fe0bfd3fe7a55be9d9df1
Tamas Toth <tamas.toth@vacuumlabs.com>
2021-01-29 15:04:33 +0100

    Lift @MockBean annotation to class level

--- app/src/test/kotlin/exchange/ExchangeApplicationTests.kt
+++ app/src/test/kotlin/exchange/ExchangeApplicationTests.kt
@@ -10,16 +10,8 @@ import org.springframework.boot.test.mock.mockito.MockBean
 
 @SpringBootTest
 @ActiveProfiles("test")
+@MockBean(classes = [UserRepository::class, BalanceRepository::class, OrderRepository::class])
 class ExchangeApplicationTests {
-
-	@MockBean
-	private lateinit var userRepository : UserRepository
-
-	@MockBean
-	private lateinit var balanceRepository : BalanceRepository
-
-	@MockBean
-	private lateinit var orderRepository : OrderRepository
 
 	@Test
 	fun contextLoads() {
